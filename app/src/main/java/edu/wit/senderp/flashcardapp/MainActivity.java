03f6d9b777eb27064cf525adb8e4965f9b5da16c
Sender <senderp@wit.edu>
2018-10-13 17:34:33 -0400

    Version 1- Basic

---
+++ app/src/main/java/edu/wit/senderp/flashcardapp/MainActivity.java
@@ -0,0 +1,25 @@
+package edu.wit.senderp.flashcardapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void showAnswer(View v) {
+        findViewById(R.id.flashcar_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcar_question).setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+}
