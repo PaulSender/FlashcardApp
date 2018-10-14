3758fc28c4802121bf4261f874bd58e957ee8d10
Paul-Sender <PaulSender99@gmail.com>
2018-10-13 20:04:18 -0400

    Version 2: all optional features added

--- app/src/main/java/edu/wit/senderp/flashcardapp/MainActivity.java
+++ app/src/main/java/edu/wit/senderp/flashcardapp/MainActivity.java
@@ -3,6 +3,7 @@ package edu.wit.senderp.flashcardapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.background).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.choice1).setBackgroundColor(getResources().getColor(R.color.questionColor));
+                findViewById(R.id.choice2).setBackgroundColor(getResources().getColor(R.color.questionColor));
+                findViewById(R.id.choice3).setBackgroundColor(getResources().getColor(R.color.questionColor));
+
+            }
+        });
+
+
 
     }
 
@@ -22,4 +34,67 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void showQuestion(View v) {
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcar_question).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+            }
+        });
+
+    }
+
+    public void wrong(View v) {
+        findViewById(R.id.choice1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.choice1).setBackgroundColor(getResources().getColor(R.color.red));
+                findViewById(R.id.choice3).setBackgroundColor(getResources().getColor(R.color.green));
+            }
+        });
+        findViewById(R.id.choice2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.choice2).setBackgroundColor(getResources().getColor(R.color.red));
+                findViewById(R.id.choice3).setBackgroundColor(getResources().getColor(R.color.green));
+            }
+        });
+
+    }
+
+    public void correct(View v) {
+        findViewById(R.id.choice3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.choice3).setBackgroundColor(getResources().getColor(R.color.green));
+            }
+        });
+    }
+    public void hide(View v) {
+        findViewById(R.id.eye_close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.Eye).setVisibility(View.VISIBLE);
+                findViewById(R.id.eye_close).setVisibility(View.INVISIBLE);
+                findViewById(R.id.choice1).setVisibility(View.INVISIBLE);
+                findViewById(R.id.choice2).setVisibility(View.INVISIBLE);
+                findViewById(R.id.choice3).setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+    public void show (View v) {
+        findViewById(R.id.Eye).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.eye_close).setVisibility(View.VISIBLE);
+                findViewById(R.id.Eye).setVisibility(View.INVISIBLE);
+                findViewById(R.id.choice1).setVisibility(View.VISIBLE);
+                findViewById(R.id.choice2).setVisibility(View.VISIBLE);
+                findViewById(R.id.choice3).setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
 }
